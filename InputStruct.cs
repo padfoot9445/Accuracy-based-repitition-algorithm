405e6a373dc58f5bca6f4b8df7f1a39b855de74b
unknown <yipy214@gmail.com>
2023-12-16 19:06:41 +0000

    Removed all interfaces from InputStruct.cs

--- InputStruct.cs
+++ InputStruct.cs
@@ -1,7 +1,14 @@
 namespace RepAlgorithm;
 public struct CorrectTotalRatio : ICorrectTotalRatio
 {
-    public int Corrects {get; private set;}
-    public int Total {get; private set;}
+    public int Corrects {get; init;}
+    public int Total {get; init;}
     public CorrectTotalRatio(int Corrects, int Total){this.Corrects = Corrects; this.Total = Total;}
+}
+
+public readonly struct InputValueGroup<T>{
+    public T Value{ get; init; }
+    public ICorrectTotalRatio Ratio{ get; init; }
+
+    public InputValueGroup(T value, ICorrectTotalRatio ratio){ Value = value; Ratio = ratio; }
 }
