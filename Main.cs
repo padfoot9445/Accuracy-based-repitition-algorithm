fc39e7a92987295e5edc3f159e21306d9e8afe0b
unknown <yipy214@gmail.com>
2023-12-16 19:10:16 +0000

    Removed Interface references and made argument inputs arrays

--- Main.cs
+++ Main.cs
@@ -3,7 +3,7 @@ namespace RepAlgorithm;
 public class RepetitionAlgorithm{
     protected static Random Rnd {get; private set;} = new();
     private readonly RatioModifier modifierCallBack;
-    public int GetIndex(ICorrectTotalRatio[] input){
+    public int GetIndex(CorrectTotalRatio[] input){
        int len = input.Length;
        float[] inputRatioArray = new float[len];
        float total = 0;
@@ -14,8 +14,8 @@ public class RepetitionAlgorithm{
        }
        return Rnd.Next(Convert.ToInt32(total));
     }
-    public T GetItemValue<T>(IInputValueGroup<T>[] input) => GetItem(input, GetIndex((ICorrectTotalRatio[])(from e in input select e.Ratio).ToArray()));
-    public static T GetItem<T>(IInputValueGroup<T>[] input, int index) => input[index].Value;
+    public T GetItemValue<T>(InputValueGroup<T>[] input) => GetItem(input, GetIndex((from e in input select e.Ratio).ToArray()));
+    public static T GetItem<T>(InputValueGroup<T>[] input, int index) => input[index].Value;
 
     public RepetitionAlgorithm(RatioModifier rmodifier){modifierCallBack = rmodifier;}
 }
