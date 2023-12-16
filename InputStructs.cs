208df725bb3e747b109de154c2822f6baa1ccce7
unknown <yipy214@gmail.com>
2023-12-16 19:09:24 +0000

    Transferred comments and *actually* removed all interface references

--- InputStructs.cs
+++ InputStructs.cs
@@ -1,14 +1,14 @@
 namespace RepAlgorithm;
 public struct CorrectTotalRatio
 {
-    public int Corrects {get; init;}
-    public int Total {get; init;}
+    public int Corrects {get; init;} //total corrects of the entire tree descending from that position
+    public int Total {get; init;} //total amount of tries in the entire tree descending
     public CorrectTotalRatio(int Corrects, int Total){this.Corrects = Corrects; this.Total = Total;}
 }
 
 public readonly struct InputValueGroup<T>{
     public T Value{ get; init; }
-    public ICorrectTotalRatio Ratio{ get; init; }
+    public CorrectTotalRatio Ratio{ get; init; }
 
-    public InputValueGroup(T value, ICorrectTotalRatio ratio){ Value = value; Ratio = ratio; }
+    public InputValueGroup(T value, CorrectTotalRatio ratio){ Value = value; Ratio = ratio; }
 }
